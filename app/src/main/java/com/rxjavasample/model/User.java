7d34190616f5e0c36098b9b447e363973947fb05
Avdhesh AKhani <avdhesh.akhani123@gmail.com>
2016-12-06 13:54:04 -0500

    Initial

---
+++ app/src/main/java/com/rxjavasample/model/User.java
@@ -0,0 +1,17 @@
+package com.rxjavasample.model;
+
+/**
+ * Created by Avdhesh AKhani on 12/6/2016.
+ */
+
+public class User {
+    private String _name;
+
+    public String getName() {
+        return _name;
+    }
+
+    public void setName(String value) {
+        _name = value;
+    }
+}
---
+++ app/src/test/java/com/rxjavasample/Presenters/CategoryFragmentPresenterImplTest.java
@@ -0,0 +1,21 @@
+package com.rxjavasample.Presenters;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Avdhesh AKhani on 12/6/2016.
+ */
+public class CategoryFragmentPresenterImplTest {
+    @Test
+    public void init() throws Exception {
+
+    }
+
+    @Test
+    public void getCategories() throws Exception {
+
+    }
+
+}
